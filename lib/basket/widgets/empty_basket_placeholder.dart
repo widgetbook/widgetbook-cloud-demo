7ea6f1b0ca151eeea93bb807dc057f6ef5cfb46c
Roaa <roaa94khaddam@gmail.com>
2023-07-20 13:13:04 +0300

    feat: implement basket widgets and screen

---
+++ lib/basket/widgets/empty_basket_placeholder.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:groceries_app/theme/app_theme.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart';
+
+@UseCase(
+  name: 'Default',
+  type: EmptyBasketPlaceholder,
+  designLink:
+  'https://www.figma.com/file/EXuEpwiyksLAejYX1qr1v4/Demo-App-featuring-variables?type=design&node-id=112-7193&mode=dev',
+)
+Widget buildEmptyBasketPlaceholderUseCase(BuildContext context) {
+  return const EmptyBasketPlaceholder();
+}
+
+class EmptyBasketPlaceholder extends StatelessWidget {
+  const EmptyBasketPlaceholder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints(
+          maxWidth: 320,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              height: 102,
+              width: 102,
+              decoration: BoxDecoration(
+                color: AppTheme.of(context).surface.light,
+                borderRadius: BorderRadius.circular(
+                  AppTheme.of(context).radius.full,
+                ),
+              ),
+              child: Icon(
+                Icons.local_mall_outlined,
+                size: 56,
+                color: AppTheme.of(context).surface.secondary,
+              ),
+            ),
+            SizedBox(
+              height: AppTheme.of(context).spacing.medium,
+            ),
+            Text(
+              AppLocalizations.of(context)!.basketEmpty,
+              style: AppTheme.of(context).typography.headingMedium24,
+            ),
+            SizedBox(
+              height: AppTheme.of(context).spacing.small,
+            ),
+            Text(
+              AppLocalizations.of(context)!.basketEmptyMessage,
+              textAlign: TextAlign.center,
+              style: AppTheme.of(context).typography.bodyRegular14,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/basket/widgets/summary.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:groceries_app/theme/app_theme.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart';
+
+@UseCase(
+  name: 'Default',
+  type: Summary,
+  designLink:
+  'https://www.figma.com/file/EXuEpwiyksLAejYX1qr1v4/Demo-App-featuring-variables?type=design&node-id=112-6510&mode=dev',
+)
+Widget buildSummaryUseCase(BuildContext context) {
+  return Summary(
+    total: context.knobs.double.slider(
+      label: 'Total',
+      initialValue: 10,
+      min: 0,
+      max: 1000,
+    ),
+    delivery: context.knobs.double.slider(
+      label: 'Delivery',
+      initialValue: 2,
+      min: 0,
+      max: 10,
+    ),
+    subTotal: context.knobs.double.slider(
+      label: 'Subtotal',
+      initialValue: 2,
+      min: 0,
+      max: 1000,
+    ),
+  );
+}
+
+class Summary extends StatelessWidget {
+  const Summary({
+    super.key,
+    required this.total,
+    required this.delivery,
+    required this.subTotal,
+  });
+
+  final double total;
+  final double delivery;
+  final double subTotal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          height: 1,
+          color: AppTheme.of(context).border.highEmphasis,
+        ),
+        SizedBox(
+          height: AppTheme.of(context).spacing.small,
+        ),
+        Row(
+          children: [
+            Text(
+              AppLocalizations.of(context)!.basketSubtotal,
+              style: AppTheme.of(context).typography.subheadingRegular16,
+            ),
+            Expanded(child: Container()),
+            Text(
+              '\$${subTotal.toStringAsFixed(2)}',
+              style: AppTheme.of(context).typography.subheadingRegular16,
+            ),
+          ],
+        ),
+        SizedBox(
+          height: AppTheme.of(context).spacing.small,
+        ),
+        Row(
+          children: [
+            Text(
+              AppLocalizations.of(context)!.basketDelivery,
+              style: AppTheme.of(context).typography.subheadingRegular16,
+            ),
+            Expanded(child: Container()),
+            Text(
+              '\$${delivery.toStringAsFixed(2)}',
+              style: AppTheme.of(context).typography.subheadingRegular16,
+            ),
+          ],
+        ),
+        SizedBox(
+          height: AppTheme.of(context).spacing.small,
+        ),
+        Row(
+          children: [
+            Text(
+              AppLocalizations.of(context)!.basketTotal,
+              style: AppTheme.of(context).typography.subheadingMedium20,
+            ),
+            Expanded(child: Container()),
+            Text(
+              '\$${total.toStringAsFixed(2)}',
+              style: AppTheme.of(context).typography.subheadingMedium20,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
